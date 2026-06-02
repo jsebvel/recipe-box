fe488c8031a9cb57d67d8ba4be68bd0b1ace90e9
Johan Sebastian Velasquez <jsebvel@gmail.com>
2026-06-02 00:05:33 -0500

    feat: initial commit

---
+++ resources/js/Components/RecipeCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed} from 'vue'
+
+const props = defineProps({
+    title: String,
+    prepMinutes: Number
+})
+
+console.log(props)
+const formattedTime = computed(() => {
+    const hours = Math.floor(props.prepMinutes / 60)
+    const mins = props.prepMinutes %60
+    return hours > 0 ? `${hours}h ${mins}m` : `${mins}m`
+})
+
+const isExpanded = ref(false)
+</script>
+
+<template>
+    <div class="border rounded p-4">
+        <h2>{{title}}</h2>
+        <p>Tiempo: {{formattedTime}}</p>
+        <button @click="isExpanded = !isExpanded">
+            {{isExpanded? "ocultar": "Mostrar más"}}
+        </button>
+        <div v-if="isExpanded">
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Recipes/Index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import RecipeCard from '@/Components/RecipeCard.vue'
+
+defineProps({
+  recipes: Array
+})
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Mis Recetas</h1>
+    <div class="grid gap-4">
+      <RecipeCard
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        :title="recipe.title"
+        :prep-minutes="recipe.prep_minutes"
+      >
+        {{ recipe.body }}
+      </RecipeCard>
+    </div>
+  </div>
+</template>
